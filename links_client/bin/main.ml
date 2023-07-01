f03c231c1305d4ce28b9c45ddb4fa7f4959d4d83
Yunbo-Glasgow <2731861p@student.gla.ac.uk>
2023-07-01 17:19:06 +0800

    unix

--- links_client/bin/main.ml
+++ links_client/bin/main.ml
@@ -1,1 +1,29 @@
-let () = print_endline "Hello, Client!"
+open Unix
+
+let client () =
+  let client_socket = socket PF_INET SOCK_STREAM 0 in
+  let server_addr = ADDR_INET (inet_addr_loopback, 8080) in
+  connect client_socket server_addr;
+  let out_chan = out_channel_of_descr client_socket in
+  let in_chan = in_channel_of_descr client_socket in
+  try
+    while true do
+      print_string "Enter message to send to server (or type 'exit' to quit): ";
+      let message = read_line () in
+      if message = "exit" then
+        raise End_of_file
+      else (
+        output_string out_chan (message ^ "\n");
+        flush out_chan;
+        let response = input_line in_chan in
+        print_endline ("Server response: " ^ response)
+      )
+    done
+  with End_of_file ->
+    close_in in_chan;
+    close_out out_chan;
+    close client_socket;
+    print_endline "Disconnected from server."
+
+let () =
+  client ()
--- links_server/bin/main.ml
+++ links_server/bin/main.ml
@@ -1,1 +1,24 @@
-let () = print_endline "Hello, Server!"
+open Unix
+
+let server () =
+  let server_socket = socket PF_INET SOCK_STREAM 0 in
+  let server_addr = ADDR_INET (inet_addr_loopback, 8080) in
+  bind server_socket server_addr;
+  listen server_socket 5;
+  print_endline "Server started, waiting for connections...";
+  while true do
+    let client_socket, _ = accept server_socket in
+    let in_chan = in_channel_of_descr client_socket in
+    try
+      while true do
+        let line = input_line in_chan in
+        print_endline ("Received from client: " ^ line);
+      done
+    with End_of_file ->
+      close_in in_chan;
+      close client_socket;
+      print_endline "Client disconnected."
+  done
+
+let () =
+  server ()
